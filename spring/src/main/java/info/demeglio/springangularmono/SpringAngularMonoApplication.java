882579fea58b0b51d0c2c912ee7d6814fa80a049
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2020-03-29 22:27:59 -0400

    initial commit

---
+++ spring/src/main/java/info/demeglio/springangularmono/SpringAngularMonoApplication.java
@@ -0,0 +1,13 @@
+package info.demeglio.springangularmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularMonoApplication.class, args);
+    }
+
+}
---
+++ spring/src/test/java/info/demeglio/springangularmono/SpringAngularMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.demeglio.springangularmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAngularMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
